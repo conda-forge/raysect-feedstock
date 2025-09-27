7519e5f718bafc1959d73a24bd46fdd9e8b71576
munechika-koyo <munechika.koyo@gmail.com>
2025-09-27 14:35:07 +0200

    MNT: Fix comment and sed command for stripping buildtype from MESON_ARGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,8 +10,8 @@ export SETUPTOOLS_SCM_PRETEND_VERSION="${PKG_VERSION}"
 export MESON_ARGS="${MESON_ARGS} --pkg-config-path=${PREFIX}/lib/pkgconfig"
 
 # meson-python already sets up a -Dbuildtype=release argument to meson, so
-# we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
-MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
+# we need to strip -Dbuildtype=release out of MESON_ARGS or fail due to redundancy
+MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/-Dbuildtype=release //g')"
 
 # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
 $PYTHON -m build -w -n -x \
